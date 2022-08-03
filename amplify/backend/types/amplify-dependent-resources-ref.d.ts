11e9d172a28d8592c699341cedf6dc9e92b1c10b
koomekevo <koome.kevo@gurucrafts.net>
2022-08-03 05:13:49 +0300

    Modify useexperimentalpipelinedtransformer to 1 and transformerversion to 1

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,8 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "jibambe": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
