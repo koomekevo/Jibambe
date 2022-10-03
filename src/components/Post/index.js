6a271b03238a08fd212c420cba010973cdcf00cb
koomekevo <koome.kevo@gurucrafts.net>
2022-10-03 08:19:12 +0300

    Add styles.video and styles.container

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import Video from 'react-native-video';
+import styles from './styles';
 
 const Post = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Video
         source={{
           uri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
         }}
+        style={styles.video}
+        resizeMode={'cover'}
       />
     </View>
   );
