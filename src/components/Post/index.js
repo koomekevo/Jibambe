140dc1d8f719f4a7d57f01d1674aac4220cde192
koomekevo <koome.kevo@gurucrafts.net>
2022-10-03 12:44:36 +0300

    Install flow static type checker

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableWithoutFeedback} from 'react-native';
 import Video from 'react-native-video';
 import styles from './styles';
 
 const Post = () => {
+  const [paused, setPaused] = useState({initialState: false});
+
+  const onPlayPausePress = () => {
+    setPaused(!paused);
+  };
+
   return (
     <View style={styles.container}>
-      <Video
-        source={{
-          uri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
-        }}
-        style={styles.video}
-        onError={(e: LoadError) => console.log(e)}
-        resizeMode={'cover'}
-        repeat={true}
-      />
+      <TouchableWithoutFeedback onPress={onPlayPausePress}>
+        <Video
+          source={{
+            uri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+          }}
+          style={styles.video}
+          onError={(e: LoadError) => console.log(e)}
+          resizeMode={'cover'}
+          repeat={true}
+          paused={paused}
+        />
+      </TouchableWithoutFeedback>
     </View>
   );
 };
