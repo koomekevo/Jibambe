bdea74685f4d9cf841a13c3f364142098fa7932d
koomekevo <koome.kevo@gurucrafts.net>
2022-10-05 09:33:18 +0300

    Add FlatList to scroll through posts

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,31 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, Dimensions} from 'react-native';
 import Post from '../../components/Post';
-
-const post1 = {
-  id: 'p1',
-  videoUri:
-    'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
-  user: {
-    id: 'u1',
-    username: 'koomekevo',
-    imageUri:
-      'https://pbs.twimg.com/profile_images/1541663888600711168/-7irn83H_400x400.jpg',
-  },
-  description: 'come my girl @caro',
-  songName: 'NF - The Search',
-  songImage:
-    'https://pbs.twimg.com/profile_images/1541663888600711168/-7irn83H_400x400.jpg',
-  likes: 123,
-  comments: 12,
-  shares: 44,
-};
+import posts from '../../../data/posts';
 
 const Home = () => {
   return (
     <View>
-      <Post post={post1} />
+      <FlatList
+        data={posts}
+        renderItem={({item}) => <Post post={item} />}
+        showsVerticalScrollIndicator={false}
+        snapToInterval={Dimensions.get('window').height}
+        snapToAlignment={'start'}
+        decelerationRate={'fast'}
+      />
     </View>
   );
 };
