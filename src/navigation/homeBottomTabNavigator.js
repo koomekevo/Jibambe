a00dab972361037958edffa87602542bb31e1bc4
koomekevo <koome.kevo@gurucrafts.net>
2022-10-06 19:03:08 +0300

    Add headerShown:false to Tab.Navigator

--- src/navigation/homeBottomTabNavigator.js
+++ src/navigation/homeBottomTabNavigator.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const Tab = createBottomTabNavigator();
 
 const HomeBottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={'Home'} component={Home} />
+    <Tab.Navigator
+      screenOptions={{
+        tabStyle: {
+          backgroundColor: '#000000',
+        },
+        headerShown: false,
+        activeTintColor: '#fff',
+      }}>
+      <Tab.Screen
+        name={'Home'}
+        component={Home}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Entypo name={'home'} size={24} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen name={'Search'} component={Home} />
       <Tab.Screen name={'Upload'} component={Home} />
       <Tab.Screen name={'Inbox'} component={Home} />
